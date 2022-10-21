ce14a24b0b958366e06ceeb0b1e28300ba410787
Helen Lehrer <hlehrer@outlook.com>
2022-10-21 10:00:05 -0700

    Add Models

--- SweetAndSavoryTreats/Controllers/HomeController.cs
+++ SweetAndSavoryTreats/Controllers/HomeController.cs
@@ -1,25 +1,25 @@
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
-using SweetAndSavoryTreats.Models;
-using System.Collections.Generic;
-using System.Linq;
+// using Microsoft.AspNetCore.Mvc.Rendering;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.AspNetCore.Mvc;
+// using SweetAndSavoryTreats.Models;
+// using System.Collections.Generic;
+// using System.Linq;
 
-namespace SweetAndSavoryTreats.Controllers
-{
-    public class HomeController : Controller
-    {
-      private readonly SweetAndSavoryTreatsContext _db;
+// namespace SweetAndSavoryTreats.Controllers
+// {
+//     public class HomeController : Controller
+//     {
+//       private readonly SweetAndSavoryTreatsContext _db;
 
-      public HomeController(SweetAndSavoryTreatsContext db)
-      {
-        _db = db;
-      }
+//       public HomeController(SweetAndSavoryTreatsContext db)
+//       {
+//         _db = db;
+//       }
 
-      public ActionResult Index()
-      {
-        ViewBag.Treat = new List<Treat>( _db.Treats);
-        return View( _db.Flavors.ToList());
-      }
-    }
-}
+//       public ActionResult Index()
+//       {
+//         ViewBag.Treat = new List<Treat>( _db.Treats);
+//         return View( _db.Flavors.ToList());
+//       }
+//     }
+// }
--- SweetAndSavoryTreats/Controllers/TreatsController.cs
+++ SweetAndSavoryTreats/Controllers/TreatsController.cs
@@ -1,113 +1,113 @@
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
-using SweetAndSavoryTreats.Models;
-using System.Collections.Generic;
-using System.Linq;
-using System;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using System.Threading.Tasks;
-using System.Security.Claims;
+// using Microsoft.AspNetCore.Mvc.Rendering;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.AspNetCore.Mvc;
+// using SweetAndSavoryTreats.Models;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System;
+// using Microsoft.AspNetCore.Authorization;
+// using Microsoft.AspNetCore.Identity;
+// using System.Threading.Tasks;
+// using System.Security.Claims;
 
-namespace SweetAndSavoryTreats.Controllers
-{
-  [Authorize]
-  public class TreatsController : Controller
-  {
-    private readonly SweetAndSavoryTreatsContext _db;
-    private readonly UserManager<ApplicationUser> _userManager;
+// namespace SweetAndSavoryTreats.Controllers
+// {
+//   [Authorize]
+//   public class TreatsController : Controller
+//   {
+//     private readonly SweetAndSavoryTreatsContext _db;
+//     private readonly UserManager<ApplicationUser> _userManager;
 
-    public TreatsController(UserManager<ApplicationUser> userManager, SweetAndSavoryTreatsContext db)
-    {
-      _userManager = userManager;
-      _db = db;
-    }
+//     public TreatsController(UserManager<ApplicationUser> userManager, SweetAndSavoryTreatsContext db)
+//     {
+//       _userManager = userManager;
+//       _db = db;
+//     }
 
-    public async Task<ActionResult> Index()
-    {
-      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-      var currentUser = await _userManager.FindByIdAsync(userId);
-      var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
-      return View(userTreats);
-    }
+//     public async Task<ActionResult> Index()
+//     {
+//       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+//       var currentUser = await _userManager.FindByIdAsync(userId);
+//       var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
+//       return View(userTreats);
+//     }
 
-    public ActionResult Create()
-    {
-      return View();
-    }
+//     public ActionResult Create()
+//     {
+//       return View();
+//     }
 
-    [HttpPost]
-    public async Task<ActionResult> Create(Treat treat, int FlavorId)
-    {
-      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-      var currentUser = await _userManager.FindByIdAsync(userId);
-      treat.User = currentUser;
-      _db.Treats.Add(treat);
-      _db.SaveChanges();
-      if (FlavorId != 0)
-      {
-        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
-      }
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+//     [HttpPost]
+//     public async Task<ActionResult> Create(Treat treat, int FlavorId)
+//     {
+//       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+//       var currentUser = await _userManager.FindByIdAsync(userId);
+//       treat.User = currentUser;
+//       _db.Treats.Add(treat);
+//       _db.SaveChanges();
+//       if (FlavorId != 0)
+//       {
+//         _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+//       }
+//       _db.SaveChanges();
+//       return RedirectToAction("Index");
+//     }
 
-    public ActionResult Details(int id)
-    {
-      var thisTreat = _db.Treats
-          .Include(Treat => Treat.JoinEntities)
-          .ThenInclude(join => join.Flavor)
-          .FirstOrDefault(Treat => Treat.TreatId == id);
-      return View(thisTreat);
-    }
+//     public ActionResult Details(int id)
+//     {
+//       var thisTreat = _db.Treats
+//           .Include(Treat => Treat.JoinEntities)
+//           .ThenInclude(join => join.Flavor)
+//           .FirstOrDefault(Treat => Treat.TreatId == id);
+//       return View(thisTreat);
+//     }
 
-    public ActionResult Edit(int id)
-    {
-      var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
-      ViewBag.FlavorId = new SelectList( _db.Flavors, "FlavorId", "FlavorName");
-      return View(thisTreat);
-    }
+//     public ActionResult Edit(int id)
+//     {
+//       var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
+//       ViewBag.FlavorId = new SelectList( _db.Flavors, "FlavorId", "FlavorName");
+//       return View(thisTreat);
+//     }
 
-    [HttpPost]
-    public ActionResult Edit(Treat treat)
-    {
-      _db.Entry(treat).State = EntityState.Modified;
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+//     [HttpPost]
+//     public ActionResult Edit(Treat treat)
+//     {
+//       _db.Entry(treat).State = EntityState.Modified;
+//       _db.SaveChanges();
+//       return RedirectToAction("Index");
+//     }
 
-    public ActionResult AddFlavor(int id)
-    {
-      var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
-      ViewBag.FlavorId = new SelectList( _db.Flavors, "FlavorId", "FlavorName");
-      return View(thisTreat);
-    }
+//     public ActionResult AddFlavor(int id)
+//     {
+//       var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
+//       ViewBag.FlavorId = new SelectList( _db.Flavors, "FlavorId", "FlavorName");
+//       return View(thisTreat);
+//     }
 
-    [HttpPost]
-    public ActionResult AddFlavor(Treat Treat, int FlavorId)
-    {
-      if (FlavorId != 0)
-      {
-        _db.FlavorTreat.Add(new FlavorTreat() { TreatId = Treat.TreatId, FlavorId = FlavorId });
-        _db.SaveChanges();
-      }
-      return RedirectToAction("Index");
-    }
+//     [HttpPost]
+//     public ActionResult AddFlavor(Treat Treat, int FlavorId)
+//     {
+//       if (FlavorId != 0)
+//       {
+//         _db.FlavorTreat.Add(new FlavorTreat() { TreatId = Treat.TreatId, FlavorId = FlavorId });
+//         _db.SaveChanges();
+//       }
+//       return RedirectToAction("Index");
+//     }
 
-    public ActionResult Delete(int id)
-    {
-      var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
-      return View(thisTreat);
-    }
+//     public ActionResult Delete(int id)
+//     {
+//       var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
+//       return View(thisTreat);
+//     }
 
-    [HttpPost, ActionName("Delete")]
-    public ActionResult DeleteConfirmed(int id)
-    {
-      var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
-      _db.Treats.Remove(thisTreat);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
-  }
-}
+//     [HttpPost, ActionName("Delete")]
+//     public ActionResult DeleteConfirmed(int id)
+//     {
+//       var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
+//       _db.Treats.Remove(thisTreat);
+//       _db.SaveChanges();
+//       return RedirectToAction("Index");
+//     }
+//   }
+// }
