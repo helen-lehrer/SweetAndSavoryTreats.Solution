0ab00f823b556e73e46cbb322cd3c8063746950f
Helen Lehrer <hlehrer@outlook.com>
2022-10-21 15:32:07 -0700

    Add remove Flavor from Treat functionality

--- SweetAndSavoryTreats/Controllers/TreatsController.cs
+++ SweetAndSavoryTreats/Controllers/TreatsController.cs
@@ -108,5 +108,13 @@ namespace SweetAndSavoryTreats.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+     [HttpPost]
+    public ActionResult RemoveFlavor(int joinId)
+    {
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
