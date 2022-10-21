113ca5c5063625ebd6be0d327e1e97fff14bbe4f
Helen Lehrer <hlehrer@outlook.com>
2022-10-21 14:10:26 -0700

    Add ViewModels

--- SweetAndSavoryTreats/ViewModels/LoginViewModel.cs
+++ SweetAndSavoryTreats/ViewModels/LoginViewModel.cs
@@ -1,8 +1,8 @@
-// namespace SweetAndSavoryTreats.ViewModels
-// {
-//   public class LoginViewModel
-//   {
-//     public string Email { get; set; }
-//     public string Password { get; set; }
-//   }
-// }
+namespace SweetAndSavoryTreats.ViewModels
+{
+  public class LoginViewModel
+  {
+    public string Email { get; set; }
+    public string Password { get; set; }
+  }
+}
